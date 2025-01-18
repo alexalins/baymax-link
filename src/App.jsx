ae58b32edbcca9c0df763c8b0a3e503865f11a53
alexalins <alexa.glinsoo@gmail.com>
2025-01-18 17:47:46 -0300

    crinado auth e configurando firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
+import Login from './pages/login/Login';
+import { onAuthStateChanged } from 'firebase/auth';
+
+//context
+import { AuthProvider } from "./context/AuthContext";
+
+//hooks
+import useAuthentication from "./hooks/useAuthentication";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Login />}></Route>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
+  );
 }
 
 export default App
